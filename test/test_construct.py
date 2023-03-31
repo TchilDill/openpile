0327f8faba815bd8ada6016a77e0381f92c071e4
TchilDill <guillaume.melin@icloud.com>
2023-03-31 22:33:10 +0200

    tests for Layer

--- test/test_construct.py
+++ test/test_construct.py
@@ -1,7 +1,9 @@
 
 from openpile import construct
 from openpile.soilmodels import API_clay
+import pytest
 class TestPile:
+
     def test_main_constructor(self):
         # create a steel and circular pile
         pile = construct.Pile.create(kind='Circular',
@@ -21,13 +23,42 @@ class TestPile:
 
 class TestLayer:
     def test_constructor(self):
-        layer = construct.Layer(name='Soft Clay',
-                        top=0,
-                        bottom=-10,
-                        weight=19,
-                        lateral_model=API_clay(Su=[30,35], eps50=[0.01, 0.02], Neq=100), 
-                    )
-        assert 1==1
+        try:
+            layer = construct.Layer(name='Soft Clay',
+                            top=0,
+                            bottom=-10,
+                            weight=19,
+                            lateral_model=API_clay(Su=[30,35], eps50=[0.01, 0.02], Neq=100), 
+                        )
+        except Exception:
+            assert False, "Constructor did not work"
+
+    def test_wrong_order_top_and_bottom(self):
+        with pytest.raises(Exception):
+            layer = construct.Layer(name='Soft Clay',
+                            top=-10,
+                            bottom=0,
+                            weight=19,
+                            lateral_model=API_clay(Su=[30,35], eps50=[0.01, 0.02], Neq=100), 
+                        )
+
+    def test_equal_top_and_bottom(self):
+        with pytest.raises(Exception):
+            layer = construct.Layer(name='Soft Clay',
+                            top=-5,
+                            bottom=-5,
+                            weight=19,
+                            lateral_model=API_clay(Su=[30,35], eps50=[0.01, 0.02], Neq=100), 
+                        )
+
+    def test_wrong_unit_weight(self):
+        with pytest.raises(Exception):
+            layer = construct.Layer(name='Soft Clay',
+                            top=0,
+                            bottom=-10,
+                            weight=10,
+                            lateral_model=API_clay(Su=[30,35], eps50=[0.01, 0.02], Neq=100), 
+                        )
 
 class TestMesh:
     def test_(self):

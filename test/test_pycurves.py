79aed519b8b1d54704389729ef3315e0c25848d7
TchilDill <guillaume.melin@icloud.com>
2023-04-23 22:43:27 +0200

    deprecated create() methods

--- test/test_pycurves.py
+++ test/test_pycurves.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
 import openpile.utils.py_curves as py
 import pytest 
+import math as m
 
+import openpile.utils.py_curves as py
 
-# @pytest.xfail
-# def test_api_sand():
-#     assert 2 == 3
+@pytest.mark.parametrize("")
+def test_api_sand():
+
+    assert 2 == 3

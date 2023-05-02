55f679b2eeb61bbbe504ab3de9f97609dd0c42e2
TchilDill <guillaume.melin@icloud.com>
2023-05-02 20:20:34 +0200

    try fixing version recognition

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
 from pathlib import Path
-
+import openpile
 
 if __name__ == "__main__":
     setup(
+        version=openpile.__version__,
         long_description=Path("README.md").read_text(encoding="utf-8"),
         long_description_content_type="text/markdown",
         project_urls={

8aa83cdf636907463ed20876011495b43a2fde4d
Guillaume Melin <gmme@cowi.com>
2023-05-17 16:49:50 +0200

    more precise conic function at small-strain

--- src/openpile/core/misc.py
+++ src/openpile/core/misc.py
@@ -132,7 +132,8 @@ def conic(
     output_length: int,
 ):
     # Create x vector with 10% extension
-    x = np.linspace(0, x_u, output_length - 1).astype(np.float32)
+    x = np.array([0, 0.005, 0.02]).astype(np.float32)*x_u
+    x = np.append(x, np.linspace(0.05*x_u, x_u, output_length - 4).astype(np.float32))
     x = np.append(x, 1.1 * x_u)
 
     a = 1 - 2 * n

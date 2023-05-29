fecfb6d85b6e6c672ddc692a8a62cc46dcf0842f
TchilDill <guillaume.melin@icloud.com>
2023-05-29 14:46:54 +0200

    clean init

--- src/openpile/__init__.py
+++ src/openpile/__init__.py
@@ -1,14 +1,3 @@
 from .globals import VERSION
 
 __version__ = VERSION
-
-
-# deprecation warning for analyses
-from . import analyze
-from warnings import warn
-
-def __getattr__(name):
-    if name == 'analyses':
-        warn("the module `analyses` has been renamed to `analyze`")
-        return analyze
-    raise AttributeError('No module named ' + name)

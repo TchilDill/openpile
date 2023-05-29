8737bf3a6e9c8cdbb1487f90ab334a29a6b4fb2b
TchilDill <guillaume.melin@icloud.com>
2023-05-29 14:46:44 +0200

    create calculate module

---
+++ src/openpile/calculate.py
@@ -0,0 +1,9 @@
+"""
+`calculate` module
+==================
+
+The `calculate` module is used to run various functions outside the scope of the `analyze` module. 
+
+Every function from this module returns an `openpile.compute.CalculateResult` object. 
+
+"""

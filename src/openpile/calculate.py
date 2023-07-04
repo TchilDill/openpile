c6f18bfedb1fbe385921ba74a8c5b893eb8af159
TchilDill <guillaume.melin@icloud.com>
2023-07-04 20:42:20 +0200

    prevent calculate.py to do anything

--- src/openpile/calculate.py
+++ src/openpile/calculate.py
@@ -19,27 +19,29 @@ def bearingcapacity(
         model,
     ) -> CalculateResult:
 
-    # initiate dataframe
-    df = model.element_properties
-    # get vertical effective stress
-    sigveff = 0.5*(model.soil_properties["sigma_v top [kPa]"]+ model.soil_properties["sigma_v bottom [kPa]"])
-    elevation = 0.5*(model.soil_properties["x_top [m]"]+model.soil_properties[ "x_bottom [m]"])
-    # depth from ground
-    depth_from_ground = (0.5*(model.soil_properties["xg_top [m]"]+model.soil_properties["xg_bottom [m]"])).abs()
-    # pile width
-    pile_width = model.element_properties["Diameter [m]"]
-    # wall thickness
-    wall_thickness = model.element_properties["Wall thickness [m]"]
+    pass
 
-    for layer in model.soil.layers:
-        elements_for_layer = model.soil_properties.loc[
-                    (model.soil_properties["x_top [m]"] <= layer.top)
-                    & (model.soil_properties["x_bottom [m]"] >= layer.bottom)
-                ].index
+    # # initiate dataframe
+    # df = model.element_properties
+    # # get vertical effective stress
+    # sigveff = 0.5*(model.soil_properties["sigma_v top [kPa]"]+ model.soil_properties["sigma_v bottom [kPa]"])
+    # elevation = 0.5*(model.soil_properties["x_top [m]"]+model.soil_properties[ "x_bottom [m]"])
+    # # depth from ground
+    # depth_from_ground = (0.5*(model.soil_properties["xg_top [m]"]+model.soil_properties["xg_bottom [m]"])).abs()
+    # # pile width
+    # pile_width = model.element_properties["Diameter [m]"]
+    # # wall thickness
+    # wall_thickness = model.element_properties["Wall thickness [m]"]
+
+    # for layer in model.soil.layers:
+    #     elements_for_layer = model.soil_properties.loc[
+    #                 (model.soil_properties["x_top [m]"] <= layer.top)
+    #                 & (model.soil_properties["x_bottom [m]"] >= layer.bottom)
+    #             ].index
         
-        if layer.axial_model is None:
-            pass
-        else:
-            # Set local layer parameters for each element of the layer
-            for i in elements_for_layer:
-                pass
+    #     if layer.axial_model is None:
+    #         pass
+    #     else:
+    #         # Set local layer parameters for each element of the layer
+    #         for i in elements_for_layer:
+    #             pass
